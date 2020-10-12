7f705a735d9881d274cd66fe424a62467b8b356f
SoVa-web <54186792+SoVa-web@users.noreply.github.com>
2020-10-13 01:09:40 +0300

    add note  in localStorage

--- js/index.js
+++ js/index.js
@@ -5,17 +5,19 @@ let notes = new Array
 function addNote(){
     let n = document.getElementById("note-title")
     let t = document.getElementById("text_note")
-    let idN = Math.floor(Math.random() * Math.floor(1000))
+    let idN = Math.floor(Math.random() * Math.floor(10000))
+    //проверка
     let noteContent = {
-        idNote: idN,
-        name: n.value,
-        text: t.value
+        "name": n.value,
+        "text": t.value
     }
     console.log(noteContent.idNote)
     notes.push(noteContent)
+    var serialObj = JSON.stringify(noteContent);
+    localStorage.setItem(idN, serialObj)
     var stringResult = ""
-    for(var i =0; i < notes.length; i++){
-        stringResult = stringResult+'<li id="li'+i+'"><h1>'+notes[i].name+'</h1>'+'<h2>'+notes[i].text+'</h2>'
+    for(var i =0; i < localStorage.length; i++){
+        stringResult = stringResult+'<li id="li'+i+'"><h1>'+localStorage.key(i).name+'</h1>'+'<h2>'+localStorage.key(i).text+'</h2>'
         +'<button class="delete-btn" id="delete_button'+i+'">Delete</button>'+'</li>'
         
     }
